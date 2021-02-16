f93e009b2e95580c97e269cff011c505cb707454
vigna-s <vigna.surapaneni@gmail.com>
2021-02-16 13:29:10 +0530

    Please debug, harness go brr

--- Problem-1/mastermind.py
+++ Problem-1/mastermind.py
@@ -3,17 +3,18 @@ import random
 
 num_of_colors = 0
 len_of_code = 0
-guess_codes = []
 turns = 0
 #Solver
+guess_codes = []
 hard_const = []
 s = Optimize()
 A = []
 
 def initialize(n,k):
+    turns = 0
     num_of_colors = n
     len_of_code = k
-    A = [ [  Int("a_%s_%s" % (i, j)) for j in range(num_of_colors) ] 
+    A = [ [  Int("A[%s][%s]" % (i, j)) for j in range(num_of_colors) ] 
       for i in range(len_of_code) ]
     for i in range(len_of_code):
         sum = 0
@@ -22,10 +23,12 @@ def initialize(n,k):
             sum = sum + A[i][j]
         hard_const.append((sum == 1))
     s.add(And(hard_const))
+    #s.check()
+    #print (s.model())
 
 def put_first_player_response(red,white):
     #add soft constraints
-    code = guess_codes[turns-1]
+    code = guess_codes[len(guess_codes)-1]
     if(red == len_of_code):
         return
     else:
@@ -35,22 +38,30 @@ def put_first_player_response(red,white):
             rsum = rsum + A[i,code[i]]
             for j in range(len_of_code):
                 rwsum = rwsum + A[j,code[i]]
-        s.add_soft(rsum == red)
-        s.add_soft(rwsum == (red+white))
+        s.add_soft((rsum == Int(red)))
+        s.add_soft(rwsum == Int(red+white))
 
 
 def get_second_player_move():
     #have to predict next move based on the constraints
-    code = []
-    if(turns == 0):
-        for i in range(len_of_code):
-            code.append(random.randint(0,num_of_colors-1))
-        guess_codes.append(code)
-        turns = turns + 1
-    else:
-        #use prev information to send out next best code?
-        turns = turns + 1
-    return code
+    
+    codelen = 0
+    s.check()
+    mod = s.model()
+    l = len(mod)
+    for i in range(l):
+        if(mod[mod[i]] == 1):
+            codelen = codelen + 1
+    gcode = [-1] * codelen
+    #print(len(gcode))
+    for i in range(l):
+        if(mod[mod[i]] == 1):
+            st = str(mod[i])
+            gcode[int(st[2])] = int(st[5])
+            
+    print(gcode)
+    guess_codes.append(gcode)
+    return gcode
 
 
 
